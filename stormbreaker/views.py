8b3e62d550d6ac4a76d285eba88758882727b094
devanshdaffodils <devansh.mittal@unthinkable.co>
2022-07-14 15:49:11 +0530

    app 2

--- stormbreaker/views.py
+++ stormbreaker/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello, world. You're at the index. change 1")
+    return HttpResponse("Hello, world. You're at the index. change 2")
